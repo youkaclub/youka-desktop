14f0a74c409f2972ed1ca1dfa15b8a0bb7c5552c
youka <youka.club@gmail.com>
2020-05-05 16:53:04 +0300

    add regex match check

--- src/lib/youtube/trending.js
+++ src/lib/youtube/trending.js
@@ -6,6 +6,7 @@ module.exports = async function () {
   const html = await rp(url);
   const re = /"url":"(\/feed\/trending\?bp=.*?)"/;
   const match = re.exec(html);
+  if (!match || match.length < 2) return [];
   const path = match[1];
   const musicURL = `https://www.youtube.com${path}`;
   const obj = await utils.initialData(musicURL);
