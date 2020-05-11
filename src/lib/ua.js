3f1613cbd86db925d19135137dca9a6df460c16b
youka <youka.club@gmail.com>
2020-05-11 09:46:50 +0300

    first time user stats

--- src/lib/ua.js
+++ src/lib/ua.js
@@ -7,3 +7,8 @@ const uaid =
   process.env.NODE_ENV === "production" && store.get("stats") ? config.ua : "";
 
 export const visitor = ua(uaid, user);
+
+const cmd = process.argv[1];
+if (cmd === "--squirrel-firstrun") {
+  visitor.event("First time", "Click", user).send();
+}
