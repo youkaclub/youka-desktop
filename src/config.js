75e630833c84c75a3a7d7e4c014e19d0bcc79019
youka <youka.club@gmail.com>
2020-05-13 00:38:02 +0300

    rm fullstory

--- src/config.js
+++ src/config.js
@@ -2,14 +2,12 @@ if (process.env.NODE_ENV === "production") {
   module.exports = {
     ua: "UA-156962391-3",
     rollbar: "8bc78e38489c483a84347df43e9d0683",
-    fullstory: "TCK6J",
     api: "https://api.audioai.online",
   };
 } else {
   module.exports = {
     ua: null,
     rollbar: null,
-    fullstory: null,
     api: "http://localhost:8000",
   };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as FullStory from "@fullstory/browser";
 import "./index.css";
 import App from "./App";
-import store from "./lib/store";
-import { fullstory as orgId } from "./config";
 import { version } from "../package.json";
 
 console.log("YOUKA_VERSION", version);
 console.log("YOUKA_GIT_SHA", process.env.REACT_APP_GIT_SHA);
-
-if (store.get("stats") && orgId) {
-  FullStory.init({ orgId });
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
