4e9f413ffa66216afed5405cb1db97d67d9ae27d
youka <youka.club@gmail.com>
2020-05-31 03:12:13 +0300

    fix subtitles-octopus-worker.wasm content-type

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 aws s3 sync --delete build s3://app.youka.club
+aws s3 cp --content-type application/wasm  build/js/subtitles-octopus-worker.wasm s3://app.youka.club/js/subtitles-octopus-worker.wasm
